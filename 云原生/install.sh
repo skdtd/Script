68ca0564edc7d9a5d206526b220d15fc8e5e8214
Zhi-Yuan Zhao <zhi-yuan.zhao@hpe.com>
2021-08-30 18:01:28 +0800

    update

--- 云原生/install.sh
+++ 云原生/install.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
 
+
+# 检查节点免密是否可用
+# 从文件读取列表
+while read line
+do
+    line=($(echo $line | grep -v "^#"))
+    if [[ -z ${line} ]];then 
+        continue 
+    fi
+    read host status user passwd <<< ${line[@]}
+    echo $host
+    echo $status
+    echo $user
+    echo $passwd
+done < $1
+
+
+
+
+
+
 # 获取版本号用的URL
 DOCKER_VERSION_URL='https://download.docker.com/linux/static/stable/x86_64/'
 KUBERNETES_VERSION_URL='https://dl.k8s.io/release/stable.txt'
 
 # 检查是否已经有现成压缩包
 # docker压缩包
-DOCKER_ARCHIVE='docker*gz'
+DOCKER_ARCHIVE=
 # k8s压缩包
-K8S_ARCHIVE='kubernetes*gz'
-K8S_BIN_LIST='kube-apiserver|kube-controller-manager|kubectl|kubelet|kube-proxy|kube-scheduler'
+K8S_ARCHIVE=
 
 
