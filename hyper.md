8b328d2c2d74630659a0e328a441c4446833985c
skdtd <zha00000@126.com>
2021-09-02 21:55:45 +0800

    update

--- hyper.md
+++
@@ -1,4 +0,0 @@
-# 获取Hyper指令列表
-Get-Command -Module hyper-v | Out-GridView
-
-https://docs.microsoft.com/zh-cn/virtualization/hyper-v-on-windows/quick-start/try-hyper-v-powershell
--- register/win10使用照片查看器.bat
+++
@@ -1,8 +0,0 @@
-@echo off&cd\&color 0a&cls
-echo 恢复Win10照片查看器
-reg add "HKLM\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /v ".jpg" /t REG_SZ /d PhotoViewer.FileAssoc.Tiff /f
-reg add "HKLM\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /v ".jpeg" /t REG_SZ /d PhotoViewer.FileAssoc.Tiff /f
-reg add "HKLM\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /v ".bmp" /t REG_SZ /d PhotoViewer.FileAssoc.Tiff /f
-reg add "HKLM\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /v ".png" /t REG_SZ /d PhotoViewer.FileAssoc.Tiff /f
-echo 请双击或右击图片，选择“照片查看器”即可
-pause
