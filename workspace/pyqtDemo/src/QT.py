fe98023abe731970688ac8aa514ca61997f4a4c5
Zhi-Yuan Zhao <zhi-yuan.zhao@hpe.com>
2021-10-22 16:56:51 +0800

    update

--- workspace/pyqtDemo/src/QT.py
+++ workspace/pyqtDemo/src/QT.py
@@ -1,23 +1,50 @@
+from _typeshed import Self
 import sys
-from test import Ui_MainWindow
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
+from PyQt5.QtCore import QPoint
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QDesktopWidget, QMainWindow, QWidget, QDialog
 
-# QtDesigner生成`的.ui文件转.py文件
+
+# ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@192.168.100.103
+
+# QtDesigner生成的.ui文件转.py文件
 # 方法1: python -m PyQt5.uic.pyuic <filename>.ui -o <filename>.py
 # 方法2: <venv>/Lib/site-packages/PyQt5/uic/pyuic.py <filename>.ui -o <filename>.py
-# ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@192.168.100.103
+
+
+# QT的三种窗口类型
+# QMainWindow, QDialog, QWidget
+# QMainWindow:  主窗口,可以包含菜单栏,工具栏和标题栏
+# QDialog:      对话框的基类,没有菜单栏,工具栏和标题栏
+# QWidget:      组件窗口
+
+
+class MainWin(QMainWindow):
+    def __init__(self, parent=None):
+        super(MainWin, self).__init__(parent)
+        self.setWindowTitle('APP')
+        self.resize(400, 300)
+        self.status = self.statusBar()
+        self.status.showMessage('I am Message!', 3000)
+        self.center()
+
+    def center(self):
+        screen = QDesktopWidget().screenGeometry()
+        size = self.geometry()
+        w = (screen.width() - size.width()) / 2
+        h = (screen.height() - size.height()) / 2
+        self.move(int(w), int(h))
 
 
 if __name__ == '__main__':
     # 创建主应用程序
     app = QApplication(sys.argv)
-    # 创建主窗口
-    mw = QMainWindow()
-    # 初始化窗口示例
-    ui = Ui_MainWindow()
-    # 创建窗口
-    ui.setupUi(mw)
+    # 设置应用图标
+    app.setWindowIcon(QIcon(''))
 
+    # 创建主窗口
+    mw = MainWin()
     # 显示窗口
     mw.show()
+
     sys.exit(app.exec_())
