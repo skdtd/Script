b055ce152909ccadf40d9cb2abfcafec0031a703
Zhi-Yuan Zhao <zhi-yuan.zhao@hpe.com>
2021-12-14 17:58:05 +0800

    11

--- notebook/机器学习/utils/util.py
+++ notebook/机器学习/utils/util.py
@@ -1,6 +1,6 @@
 import numpy as np
 
 def getBeans(n):
-    xs = np.msort(np.random.uniform(low=1,high=10,size=n))
-    ys = np.msort(np.random.uniform(low=1,high=3,size=n))
+    xs = np.msort(np.random.uniform(low=0,high=10,size=n))
+    ys = np.msort(np.random.uniform(low=0,high=3,size=n))
     return xs, ys
