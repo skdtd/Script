41f3385d326922c8fed1436e9a5a042d55e14ff6
Zhi-Yuan Zhao <zhi-yuan.zhao@hpe.com>
2022-01-06 18:00:05 +0800

    Update run.py

--- notebook/脚本/run.py
+++ notebook/脚本/run.py
@@ -18,8 +18,8 @@ class RegexConverter(BaseConverter):
     def to_python(self, value):
         return value
 
+
 app = Flask(__name__)
-base_url = ''
 app.url_map.converters['re'] = RegexConverter
 all_url = '/<re(".*"):path>'
 
@@ -28,11 +28,13 @@ all_url = '/<re(".*"):path>'
 def method_get(path=None):
     return handler(path, request.method)
 
+
 def handler(path, method):
     return method + ': ' + path + '\n'
     
 
 if __name__ == '__main__':
+    # app.run('', port=5000, debug=True)
     try:
         WSGIServer(('0.0.0.0', 5000), app).serve_forever()
     except KeyboardInterrupt:
